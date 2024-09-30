84b5009a4e5c82aa4bd52414f736802f28ac1554
= <=>
2024-09-30 12:07:04 -0700

    Geometri

---
+++ segitiga.py
@@ -0,0 +1,9 @@
+print("================================================")
+print("\t\tRUMUS SEGITIGA")
+print("================================================")
+
+1/2
+alas = int(input("masukkan alas: "))
+tinggi = int(input("masukkan tinggi: "))
+
+print("masukkan hasil: ", 1/2 * alas * tinggi)
