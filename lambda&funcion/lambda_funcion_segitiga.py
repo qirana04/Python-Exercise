b7ddddecc0e2d88f2b11b79a86c787f5e0e561cc
= <=>
2024-11-14 08:40:02 -0800

    Lambda dan Functions

---
+++ lambda&funcion/lambda_funcion_segitiga.py
@@ -0,0 +1,10 @@
+def persegi():
+    sisi = int(input('sisi\t\t: '))
+    luas = lambda s: s * s
+    keliling = lambda s: 4 * s
+
+    print('luas\t\t: ',luas(sisi), 'cm2')
+    print('keliling\t: ',keliling(sisi), 'cm')
+
+persegi()
+persegi()
